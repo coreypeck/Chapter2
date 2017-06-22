60a74ded05687f09a57c53522f407ccfb51e8541
Corey Peck <coreyjpeck@gmail.com>
2017-06-21 21:49:25 -0500

    Updated wording

---
+++ PartyInvitesChtp2/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvitesChtp2.Models;
+
+namespace PartyInvitesChtp2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Evening";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                //TODO: Email response to the party organizer
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                //there is a validation error
+                return View();
+            }
+        }
+    }
+}
---
+++ PartyInvitesChtp2/Models/GuestResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvitesChtp2.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your Name")]
+        public string Name {get; set;}
+        [Required(ErrorMessage = "Please enter your Email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your Phone Number")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Please specify if you will attend or not")]
+        public bool? WillAttend { get; set; }
+    }
+}
